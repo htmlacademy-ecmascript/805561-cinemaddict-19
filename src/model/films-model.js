03a7f03ce04c33c150ab7ce63e8be56d4b4540f8
brakozyawr <brakozyawr@yandex.ru>
2023-01-13 12:36:58 +0300

    Линтер прикопался

--- src/model/films-model.js
+++ src/model/films-model.js
@@ -1,4 +1,4 @@
-import {generateComment, generateFilm} from '../mock/film.js';
+import {generateFilm} from '../mock/film.js';
 
 export default class FilmsModel {
   #films = Array.from({length: 5}, generateFilm);
